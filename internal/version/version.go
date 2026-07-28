aac05ef21334ca48832798d6e60b5699a6edbc09
Chintan Patel <216989679+Chintanpatel24@users.noreply.github.com>
2026-07-28 20:00:23 +0530

    Change application version to v2.0.0
    Updated the application version to v2.0.0.

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,5 +2,4 @@ package version
 
 // Version is the application version string.
 // It can be overridden at compile time via linker flags:
-// go build -ldflags "-X github.com/Chintanpatel24/Matt/internal/version.Version=v1.1.0"
 var Version = "v2.0.0"
