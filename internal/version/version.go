3f529c945f2d80f4197c1969e5f70df1e7726589
Chintan Patel <216989679+Chintanpatel24@users.noreply.github.com>
2026-07-30 20:16:12 +0530

    Update version.go

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,4 +4,4 @@ package version
 // It is automatically injected at build time via -ldflags:
 // go build -ldflags "-X github.com/Chintanpatel24/Matt/internal/version.Version=v1.1.0"
 // Or updated automatically by the GitHub release workflow when a release tag is pushed.
-var Version = "v1.1.0"
+var Version = "v1.1.1"
